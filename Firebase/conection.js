9cd0f0fad3b4fc45b5144dc901d6aaf8953e25ae
maavalim <mariaclarachiuchi@gmail.com>
2025-04-29 17:27:21 -0300

    comite35

---
+++ Firebase/conection.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCraeIQ0eWGBpHpR7k-mYJeLsVJaN0E57A",
+  authDomain: "projetotarde-ce703.firebaseapp.com",
+  projectId: "projetotarde-ce703",
+  storageBucket: "projetotarde-ce703.firebasestorage.app",
+  messagingSenderId: "371649435917",
+  appId: "1:371649435917:web:ecfed118827b8692483844"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
--- src/screens/Register.js
+++ src/screens/Register.js
@@ -1,44 +1,74 @@
 import { useState, useEffect } from 'react';
 import styles from './Register.module.css';
+import { useAuthentication } from './hooks/userAuthention';
  
 const Register = () => {
-    const [displayName, setName]=useState('');
-    const [displayEmail, setEmail]  = useState('');
-    const [displayPassword, setPassword]  = useState('');
-    const [displayConfirm, setConfirm]  = useState('');
+    const [displayName, setName] = useState('');
+    const [displayEmail, setEmail] = useState('');
+    const [displayPassword, setPassword] = useState('');
+    const [displayConfirm, setConfirm] = useState('');
     const [error, setError] = useState('');
-
+    const {createUser, error: authError, loading } = useAuthentication();
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+ 
+        setError("")
+ 
+        const user = {
+            displayName,
+            displayEmail,
+            displayPassword
+        }
+ 
+        if (displayPassword !== displayConfirm) {
+            setError("As senhas precisam ser iguais!")
+            return
+        }
+        const res= await createUser (user);
+    }
     return (
-        <div className="cadastro">
+        <div>
             <h2>Cadastre-se para ter Acesso ao Site</h2>
-            <form>
+            <form onSubmit={handleSubmit}>
                 <label>
                     <span>
                         Nome:
                     </span>
-                    <input type="text" name="displayName" required placeholder="Nome do Usuário" />
+                    <input type="text" name="displayName" required placeholder="Nome do Usuário"
+                        value={displayName}
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </label>
                 <label>
                     <span>
                         E-mail:
                     </span>
-                    <input type="email" name="displayName" required placeholder="E-mail" />
+                    <input type="email" name="displayName" required placeholder="E-mail"
+                        value={displayEmail}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+ 
                 </label>
                 <label>
                     <span>
                         Senha:
                     </span>
-                    <input type="password" name="displayName" required placeholder="Senha" />
+                    <input type="password" name="displayName" required placeholder="Senha"
+                        value={displayPassword}
+                        onChange={(e) => setPassword(e.target.value)}
+                    />
                 </label>
                 <label>
                     <span>
                         Confirmar Senha:
                     </span>
-                    <input type="password" name="displayName" required placeholder="Repetir Senha" />
+                    <input type="password" name="displayName" required placeholder="Repetir Senha"
+                        value={displayConfirm}
+                        onChange={(e) => setConfirm(e.target.value)}
+                    />
                 </label>
-                 <button className="btnCadastrar">
-                    Cadastrar
-                </button>
+                <button className={styles.btnCadastrar}>Cadastro</button>
+                {error && <p className="error">{error}</p>}
             </form>
         </div>
     )
